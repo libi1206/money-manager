c2f583b56f433eff99745bdfbf1ef431910f8639
libi <843298330@qq.com>
2019-02-22 21:18:21 +0800

    修复了属性名字和getter，setter对应的BUG

--- src/main/java/com/libi/entity/FamilyMapping.java
+++ src/main/java/com/libi/entity/FamilyMapping.java
@@ -27,11 +27,11 @@ public class FamilyMapping extends BaseEntity {
         this.familyId = familyId;
     }
 
-    public Boolean getAdmin() {
+    public Boolean getIsAdmin() {
         return isAdmin;
     }
 
-    public void setAdmin(Boolean admin) {
+    public void setIsAdmin(Boolean admin) {
         isAdmin = admin;
     }
 }
