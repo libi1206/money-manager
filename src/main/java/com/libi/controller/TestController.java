67d1894c122bc5bf5c658467b880d8f4586084ac
libi <843298330@qq.com>
2019-02-25 23:15:23 +0800

    The interface for test have been done

---
+++ src/main/java/com/libi/controller/TestController.java
@@ -0,0 +1,57 @@
+package com.libi.controller;
+
+import com.libi.commons.ResponseTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping("/test")
+public class TestController {
+    @RequestMapping(value = "/get", method = RequestMethod.GET)
+    @ResponseBody
+    public ResponseTemplate getTest(HttpServletRequest request) {
+        return testMethod(request);
+    }
+
+    @RequestMapping(value = "/post", method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseTemplate postTest(HttpServletRequest request) {
+        return testMethod(request);
+    }
+
+    private ResponseTemplate testMethod(HttpServletRequest request) {
+        String param = request.getParameter("param");
+        TestBean testBean = new TestBean();
+        testBean.setCurrentTime(System.currentTimeMillis());
+        testBean.setParam(param);
+        ResponseTemplate<TestBean> responseTemplate = new ResponseTemplate<TestBean>();
+        responseTemplate.setMessage("成功");
+        responseTemplate.setData(testBean);
+        return responseTemplate;
+    }
+}
+
+class TestBean{
+    private String param;
+    private Long currentTime;
+
+    public String getParam() {
+        return param;
+    }
+
+    public void setParam(String param) {
+        this.param = param;
+    }
+
+    public Long getCurrentTime() {
+        return currentTime;
+    }
+
+    public void setCurrentTime(Long currentTime) {
+        this.currentTime = currentTime;
+    }
+}
